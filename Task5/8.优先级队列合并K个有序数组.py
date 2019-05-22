d219078195cdf20705ba4ae33eed9a02e09f20ae
Liheng <624260029@qqc.om>
2019-05-22 15:35:23 +0800

    Task5.8

---
+++ Task5/8.优先级队列合并K个有序数组.py
@@ -0,0 +1,57 @@
+class MergeSortedK(object):
+    def __init__(self, heap):
+        self.heap = heap
+        self.sorted = []
+        self.heap_size = len(self.heap)
+
+    @staticmethod
+    def left(i):
+        return 2 * i + 1
+
+    @staticmethod
+    def right(i):
+        return 2 * i + 2
+
+    def min_heap(self, i):
+
+        while True:
+            left = self.left(i)
+            right = self.right(i)
+            if right < self.heap_size and self.heap[right][0] < self.heap[i][0]:
+                min_index = right
+            else:
+                min_index = i
+
+            if left < self.heap_size and self.heap[left][0] < self.heap[min_index][0]:
+                min_index = left
+
+            else:
+                pass
+
+            if min_index == i:
+                break
+
+            self.heap[i], self.heap[min_index] = self.heap[min_index], self.heap[i]
+
+    def build_heap(self):
+        for i in range(self.heap_size // 2 - 1, -1, -1):
+            self.min_heap(i)
+
+    def merge(self):
+        if not self.heap:
+            return self.sorted
+        if self.heap:
+            self.build_heap()
+            tmp = self.heap[0].pop(0)
+            if not self.heap[0]:
+                self.heap.pop(0)
+                self.heap_size -= 1
+            self.sorted.append(tmp)
+
+        self.merge()
+
+
+sorted_K = [[1, 2, 3, 4, 5, 6], [4, 5, 6, 7, 8, 9], [10, 11, 12, 13, 14]]
+merge_sorted = MergeSortedK(sorted_K)
+merge_sorted.merge()
+print(merge_sorted.sorted)
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-string = raw_input()
+# string = raw_input()
+string = "10+1 11+2"
 attack, defense = string.split()
 X, Y = attack.split("+")
 H, D = defense.split("+")
@@ -7,8 +8,20 @@ Y = int(Y)
 H = int(H)
 D = int(D)
 count = 0
-for y in range(Y+1):
-    for d in range(D+1):
-        if y >= H-X+d:
-            count += 1
-print round(count / (Y+1) / (D+1), 2)
+
+total = (D + 1) * (Y + 1)
+
+if X >= H:
+    if X >= (H + D):
+        result = total
+
+    else:
+        result = total - (H + D - X) * (1 + H + D - X) // 2
+
+else:
+    if H > (X + Y):
+        result = 0
+    else:
+        result = (X + Y - H + 1) * (1 + X + Y - H + 1) // 2
+
+print(round(result / total, 2))
